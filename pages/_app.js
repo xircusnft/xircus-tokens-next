15db9457733c9357bb972dde242a9778b8e92af8
jmxircus <jm.xircus@gmail.com>
2021-08-10 13:09:47 +0800

    init set up

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import {ChakraProvider, extendTheme} from '@chakra-ui/react';
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+const theme = extendTheme({config})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>)
 }
 
 export default MyApp
