b08837155d18f987a594ee6f03897eea7387b255
jmxircus <jm.xircus@gmail.com>
2021-08-13 15:18:29 +0800

    filter chains

--- pages/chains/index.js
+++ pages/chains/index.js
@@ -1,14 +1,22 @@
 import { Box, Container, Heading, Input, VStack } from "@chakra-ui/react";
 import chainListLocal from "../../services/chainListLocal";
 import { useRouter } from "next/router";
+import { useMemo, useState } from "react";
 
 function Chains({ chains }) {
   const router = useRouter();
+  const [search, setSearch] = useState("");
 
   const handleSelected = (cid) => {
     console.log(cid);
     router.push(`/chains/${encodeURIComponent(cid)}`);
   };
+
+  const results = useMemo(
+    () =>
+      search.length > 2 ? chains.filter((o) => JSON.stringify(o.name).search(new RegExp(search, "ig")) > -1) : chains,
+    [search]
+  );
 
   return (
     <Container>
@@ -16,13 +24,13 @@ function Chains({ chains }) {
         List Of Chains
       </Heading>
       <Box my={2}>
-        <Input placeholder="Search Blockchain" />
+        <Input placeholder="Search Blockchain" value={search} onChange={(e) => setSearch(e.target.value)} />
       </Box>
       <Box maxHeight="80vh" overflowY="scroll">
         {" "}
         <VStack mt={5} align="stretch" spacing={2}>
-          {Array.isArray(chains) &&
-            chains.map((elem, index) => (
+          {Array.isArray(results) &&
+            results.map((elem, index) => (
               <Box
                 key={index}
                 as="button"
